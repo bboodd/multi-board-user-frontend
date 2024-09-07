b847cf34338be222241e480eb6fdddf0aab28cfd
bboodd <gkrgus9@gmail.com>
2024-09-07 13:36:53 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  //
+  const asb = () => {
+    a = 2
+    return 1
+  }
+</script>
